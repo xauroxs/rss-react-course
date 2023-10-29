7e493dfc179f0c94e9100ea2a9f001689f4c7454
xauroxs <xauroxs@gmail.com>
2023-10-29 15:38:33 +0300

    refactor: clean up starter project

---
+++ src/app/app.component.tsx
@@ -0,0 +1,11 @@
+import { Component, ReactNode } from "react";
+
+import "./app.styles.scss";
+
+class App extends Component {
+  render(): ReactNode {
+    return <div></div>;
+  }
+}
+
+export default App;
